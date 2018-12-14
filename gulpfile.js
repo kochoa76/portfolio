0470c512de8191ed05f30e6d9402b01ddec2baf7
github username <github email address>
2018-12-14 16:04:05 -0600

    add edits to mobile view of home page for portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("docs/.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['js','serve']);
